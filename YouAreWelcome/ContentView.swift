3622e18bef08dcd71ad5d884576702413d1105c8
Shailendra Nain <shailnain.6@gmail.com>
2025-05-04 21:08:18 -0400

    Initial Commit

---
+++ YouAreWelcome/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  YouAreWelcome
+//
+//  Created by Shailendra Nain on 5/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ YouAreWelcome/YouAreWelcomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreWelcomeApp.swift
+//  YouAreWelcome
+//
+//  Created by Shailendra Nain on 5/4/25.
+//
+
+import SwiftUI
+
+@main
+struct YouAreWelcomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
