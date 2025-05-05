68b3bbfaae9e30466f198c552d7899c71ccbb4b2
Shailendra Nain <shailnain.6@gmail.com>
2025-05-04 21:29:49 -0400

    backgound added

--- YouAreWelcome/ContentView.swift
+++ YouAreWelcome/ContentView.swift
@@ -9,30 +9,38 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Text("What is Football to You?")
-                .font(.largeTitle)
-                .fontWeight(.light)
-                .foregroundStyle(.green)
-            HStack{
-                Image(systemName: "figure.american.football")
-                    .resizable()
-                    .scaledToFit()
-                    .foregroundStyle(.blue)
-                
-                Image(systemName: "figure.australian.football")
-                    .resizable()
-                    .scaledToFit()
-                    .foregroundStyle(.indigo)
-                
-                Image(systemName: "figure.soccer")
-                    .resizable()
-                    .scaledToFit()
-                    .foregroundStyle(.purple)
+        ZStack {
+            Color(.gray).opacity(0.5)
+                .ignoresSafeArea()
+            
+            VStack {
+                Text("What is Football to You?")
+                    .font(.largeTitle)
+                    .fontWeight(.light)
+                    .foregroundStyle(.black)
+                HStack{
+                    Image(systemName: "figure.american.football")
+                        .resizable()
+                        .scaledToFit()
+                        .foregroundStyle(.blue)
+                    
+                    Image(systemName: "figure.australian.football")
+                        .resizable()
+                        .scaledToFit()
+                        .foregroundStyle(.indigo)
+                    
+                    Image(systemName: "figure.soccer")
+                        .resizable()
+                        .scaledToFit()
+                        .foregroundStyle(.purple)
+                }
+                .padding()
             }
+            .background()
+            .cornerRadius(10)
             .padding()
         }
-        .padding()
+        
     }
 }
 
