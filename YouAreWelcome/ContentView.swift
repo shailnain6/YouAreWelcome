6fa217ec2a1ce73869d66e22b5bc9c7d750aa2cf
Shailendra Nain <shailnain.6@gmail.com>
2025-05-05 21:03:48 -0400

    Button and Image modifier added

--- YouAreWelcome/ContentView.swift
+++ YouAreWelcome/ContentView.swift
@@ -11,18 +11,22 @@ struct ContentView: View {
     @State private var isClickedMe = false
     var body: some View {
         VStack {
+            Spacer()
             Image(systemName: "swift")
                 .resizable()
                 .scaledToFit()
                 .foregroundColor(isClickedMe ? /*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/ : .orange)
+                .frame(width: 200, height: 200)
             Text(isClickedMe ? "You Are Welcome!" : "Learn to code..")
                 .font(.title)
                 .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+            Spacer()
             Button(isClickedMe ? "Reset" : "Click Me!") {
                 isClickedMe.toggle()
             }
-            .buttonStyle(.bordered)
-            .foregroundStyle(isClickedMe ? /*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/ : .orange)
+            .buttonStyle(.borderedProminent)
+            .tint(isClickedMe ? .blue : .orange)
+            .font(.title2)
             .fontWeight(.bold)
         }
         .padding()
