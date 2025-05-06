576df59ba6cce0ed6bd34a14dfcd3fe99fdf2f71
Shailendra Nain <shailnain.6@gmail.com>
2025-05-05 20:38:36 -0400

    Button Added

--- YouAreWelcome/ContentView.swift
+++ YouAreWelcome/ContentView.swift
@@ -8,11 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isClickedMe = false
     var body: some View {
         VStack {
-            Text("You Are Welcome")
+            Image(systemName: "swift")
+                .resizable()
+                .scaledToFit()
+                .foregroundColor(isClickedMe ? /*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/ : .orange)
+            Text(isClickedMe ? "You Are Welcome!" : "Learn to code..")
+                .font(.title)
+                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+            Button(isClickedMe ? "Reset" : "Click Me!") {
+                isClickedMe.toggle()
+            }
+            .buttonStyle(.bordered)
+            .foregroundStyle(isClickedMe ? /*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/ : .orange)
+            .fontWeight(.bold)
         }
-        
+        .padding()
     }
 }
 
--- YouAreWelcome/YouAreWelcomeApp.swift
+++ YouAreWelcome/YouAreWelcomeApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct YouAreWelcomeApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            FootBallView()
         }
     }
 }
